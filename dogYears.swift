920e279a1c1ed2cae05183a75e2f58c55f82177f
teachandlearn <60556876+teachandlearn@users.noreply.github.com>
2020-02-22 10:07:41 -0600

    Base Codecademy project

---
+++ dogYears.swift
@@ -0,0 +1,20 @@
+// All credit to Codecademy
+
+// Declaring all variables
+var dogAge: Int = 10
+var earlyYears: Int = 0
+var laterYears: Int = 0
+var humanYears = 0
+
+//Early Years assigned a value
+earlyYears = 21
+
+//Later years is calculated
+laterYears = (dogAge - 2) * 4
+
+//early years and later years together
+//makes human years
+humanYears = earlyYears + laterYears
+
+// Cute little printoff
+print("My name is Butch. Ruff, ruff, I am \(humanYears) years old in human years.")
